a560238d6b3a2175dd67a1605897994e592c90ff
chenwang <316009612@qq.com>
2017-12-28 23:42:27 +0800

    导入工程

---
+++ wolfmovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wolfmovie
+//
+//  Created by WangChen on 2017/12/28.
+//  Copyright © 2017年 ChenWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
